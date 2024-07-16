3d8e6c176a3d3f1c3855947c8e3e9fbbb536c254
Dmitriy Vasilev <d.vasilev@mk.dunice.net>
2024-07-16 10:59:43 +0300

    Second Commit

--- src/main/java/com/example/BasicServer/controller/userController.java
+++ src/main/java/com/example/BasicServer/controller/userController.java
@@ -1,4 +1,11 @@
 package com.example.BasicServer.controller;
 
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/users")
 public class userController {
+
 }
